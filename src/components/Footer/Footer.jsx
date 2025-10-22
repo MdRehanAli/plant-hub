ef9c4f13a35e7159a8f232d52814f6449dcc3e70
Md Rehan Ali <rehanali21d@gmail.com>
2025-10-22 23:12:37 +0600

    Added Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import { SiPiapro } from 'react-icons/si';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is footer</h1>
-        </div>
+        <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
+            <nav className="grid grid-flow-col gap-4">
+                <a className="link link-hover">About</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Privacy Policy</a>
+                <a className="link link-hover"></a>
+            </nav>
+            <nav>
+                <div className="grid grid-flow-col gap-4 text-5xl">
+                    <a className='p-1 rounded-full hover:bg-green-300'>
+                        <FaInstagram></FaInstagram>
+                    </a>
+                    <a className='p-1 rounded-full hover:bg-green-300'>
+                        <FaFacebook></FaFacebook>
+                    </a>
+                    <a className='p-1 rounded-full hover:bg-green-300'>
+                        <SiPiapro></SiPiapro>
+                    </a>
+                </div>
+            </nav>
+            <aside>
+                <p>Copyright © {new Date().getFullYear()} - PlantHub. All rights reserved.</p>
+            </aside>
+        </footer>
     );
 };
 
